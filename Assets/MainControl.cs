9ea2e7e442406fef20fd5dbf1be67a69abd80d70
Andrew Erskine <erskine94@gmail.com>
2019-10-04 19:11:57 -0700

    Initial commit
    here we go

---
+++ Assets/MainControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MainControl : MonoBehaviour
+{
+
+    public Tilemap gameTiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int tileCount = 0;
+        foreach (Vector3Int pos in gameTiles.cellBounds.allPositionsWithin)
+        {
+            //Debug.Log(pos);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Get camera to world point
+            Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // what is index of this cell
+            Vector3Int selectedCell = gameTiles.WorldToCell(point);
+            Debug.Log(selectedCell);
+
+            // get tile object occupied by this cell
+            Debug.Log(gameTiles.GetTile(selectedCell));
+
+            WorldTile tile = (WorldTile) gameTiles.GetTile(selectedCell);
+            Debug.Log(tile.cost);
+
+            tile.DoSomeAction();
+        }
+    }
+}
---
+++ Assets/WorldTile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "New World Tile", menuName = "Tiles/World Tile")]
+public class WorldTile : Tile
+{
+    public int cost;
+    public string actionText;
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        base.GetTileData(position, tilemap, ref tileData);
+    }
+
+    public void DoSomeAction()
+    {
+        Debug.Log(actionText);
+    }
+}
